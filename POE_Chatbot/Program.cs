bfba3ae54c211ab558a950742b197f48d30f27df
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-05-24 21:08:02 +0200

    // Add ASCII Art and welcome audio

--- POE_Chatbot/Program.cs
+++ POE_Chatbot/Program.cs
@@ -16,8 +16,45 @@ namespace POE_Chatbot
             Rate = 0
         };
 
+        static Random random = new Random();
         static void Main(string[] args)
         {
+            // Play audio greeting
+            PlayWelcomeAudio("Welcome_greeting.wav");
+
+            // Creating a console window
+            Console.Title = "Cybersecurity Awareness Chatbot";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(new string('*', Console.WindowWidth));
+
+
+            //ASCII ART
+            String[] AsciiArtLines = new String[]
+{
+    "  _______     ______  ______ _____  _____  ______ ______ ______ _   _ _____  ______ _____   _____ ",
+    "  / ____\\ \\   / /  _ \\|  ____|  __ \\|  __ \\|  ____|  ____|  ____| \\ | |  __ \\|  ____|  __ \\ / ____|",
+    " | |     \\ \\_/ /| |_) | |__  | |__) | |  | | |__  | |__  | |__  |  \\| | |  | | |__  | |__) | (___  ",
+    " | |      \\   / |  _ <|  __| |  _  /| |  | |  __| |  __| |  __| | . ` | |  | |  __| |  _  / \\___ \\ ",
+    " | |____   | |  | |_) | |____| | \\ \\| |__| | |____| |    | |____| |\\  | |__| | |____| | \\ \\ ____) | ",
+    "  \\_____|  |_|  |____/|______|_|  \\_\\_____/|______|_|    |______|_| \\_|_____/|______|_|  \\_\\_____/  ",
+    "",
+    "[CBS BOT - Your Cybersecurity Buddy!]"
+};
+
+
+            int consoleWidth = Console.WindowWidth;
+            foreach (string line in AsciiArtLines)
+            {
+                int padding = (consoleWidth - line.Length) / 2;
+                Console.WriteLine(new string(' ', Math.Max(0, padding)) + line);
+            }
+
+            Console.WriteLine(new string('*', Console.WindowWidth)); // Bottom border
+
+            TypingEffect("Hello! Welcome to your Cybersecurity Awareness Assistant!\n");
+
+            // Ask for the user's name
+
         }
     }
 }
