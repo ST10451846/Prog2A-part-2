7cda1a383fd969122320a66621b6261d13486a57
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-05-24 21:06:37 +0200

    // Add speech synthesizer

--- POE_Chatbot/Program.cs
+++ POE_Chatbot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,13 @@ namespace POE_Chatbot
 {
     internal class Program
     {
+        static List<string> chatHistory = new List<string>();
+        static SpeechSynthesizer synth = new SpeechSynthesizer
+        {
+            Volume = 100,
+            Rate = 0
+        };
+
         static void Main(string[] args)
         {
         }
